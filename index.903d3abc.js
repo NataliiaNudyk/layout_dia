5632aeae33e4b50ca3da0d3eb0c8906271caae2e
Nataliia Nudyk <nataliianudyk@gmail.com>
2024-12-17 10:19:21 +0200

    make build

---
+++ index.903d3abc.js
@@ -0,0 +1,2 @@
+let e=1;!function(l){let t=document.getElementsByClassName("slider__img");1>t.length&&(e=1);for(let e=0;e<t.length;e++)t[e].style.display="none";t[e-1].style.display="block"}(1);
+//# sourceMappingURL=index.903d3abc.js.map
